cada0ef7da7ed87bddf20963e2ae082d56d5c966
Amir Mohammad Hl <dev.imorate@gmail.com>
2021-07-13 12:09:58 +0430

    Add precedence for conflict serializable transactions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import file.FileParser;
 import file.exceptions.FileNotFoundException;
 import model.Operation;
+import model.Precedence;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +18,7 @@ public class Main {
         assert fileParser != null;
         List<Operation> operationList = fileParser.getOperationList();
         Algorithm algorithm = new Algorithm(operationList);
+        Set<Precedence> precedenceSet = algorithm.getPrecedenceSet();
         System.out.println("Operation List=\t" + algorithm.getOperationList());
         System.out.println("Precedence Set=\t" + algorithm.getPrecedenceSet());
         System.out.println("Precedence Graph=\t" + algorithm.getPrecedenceGraph());
@@ -23,6 +26,9 @@ public class Main {
         ProcessResult processResult = algorithm.getSerializabilityResult();
         if (processResult.isConflictSerializable()) {
             System.out.println("Transactions are Conflict-Serializable and also View-Serializable.");
+            for (Precedence precedence : precedenceSet) {
+                System.out.println(precedence);
+            }
         } else {
             System.out.println("Transactions are not conflict serializable.");
             System.out.println("There is an edge between T" + processResult.getNodes()[0].getLabel() + " and T"
